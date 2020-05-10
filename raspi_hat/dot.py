af582867da9c583e2bff04fa1f9ec19d78293b19
alejandronespereira <alejandronespereira@gmail.com>
2020-05-10 12:13:16 +0200

    Added script with dot and physics

--- raspi_hat/dot.py
+++ raspi_hat/dot.py
@@ -37,18 +37,20 @@ class Dot(Entity):
     def setColor(self,color):
         self.matrix.data[self._position[0],self._position[1]] = self._color
 
-
     def isInLimits(self,value):
         return value >= self._limits[0] and value <= self._limits[1]
 
     def placeAt(self,new_position):
-        x = max(min(new_position[0],8),0)
-        y = max(min(new_position[1],8),0)
-        
-        self._position = [x,y]
+        x = new_position[0]
+        y = new_position[1]
+            
+        self._position = [int(x),int(y)]
         self.update()
 
     def update(self):
         self._matrix.mask[:,:] = False
-        self._matrix.mask[self._position[0],self._position[1]] = True
-        self.setColor(self._color)
+        x = self._position[0]
+        y = self._position[1]
+        if x >= 0 and x <=7 and y >= 0 and y <= 7:
+            self._matrix.mask[x,y] = True
+            self.setColor(self._color)
